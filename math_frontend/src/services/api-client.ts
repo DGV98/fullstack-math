35e434fa1e752f8e86d11e159eaa267aa572f90f
DGV98 <davidgonzalez1019@gmail.com>
2025-02-19 19:04:34 -0600

    Corrected endpoint

--- math_frontend/src/services/api-client.ts
+++ math_frontend/src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://vercel.com/dgv98s-projects/fullstack-math/6JZetRWu4hUokbNK7BsFgho8a5Ey",
+  baseURL: "https://fullstack-math.vercel.app/",
 });
